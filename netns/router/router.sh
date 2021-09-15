c24059478931ff4fcf32ab03f3903f22aa5e2ffd
hwipl <33433250+hwipl@users.noreply.github.com>
2021-09-15 12:33:49 +0200

    add netns/router/router.sh
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ netns/router/router.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# commands
+IP=/usr/bin/ip
+SYSCTL=/usr/bin/sysctl
+
+# name of network namespaces
+NS_HOST1="router-host1"
+NS_HOST2="router-host2"
+NS_ROUTER="router-router1"
+
+# veth interfaces
+VETH_HOST11="veth1"
+VETH_HOST12="veth2"
+VETH_HOST21="veth3"
+VETH_HOST22="veth4"
+
+# mac addresses
+MAC_HOST11="0a:bc:de:f0:00:11"
+MAC_HOST12="0a:bc:de:f0:00:12"
+MAC_HOST21="0a:bc:de:f0:00:21"
+MAC_HOST22="0a:bc:de:f0:00:22"
+
+# ipv4 addresses
+IPV4_HOST1="192.168.1.2/24"
+IPV4_HOST2="192.168.2.2/24"
+IPV4_ROUTER1="192.168.1.1/24"
+IPV4_ROUTER2="192.168.2.1/24"
+
+# create network namespaces
+function create_namespaces {
+	echo "Creating network namespaces..."
+	$IP netns add $NS_HOST1
+	$IP netns add $NS_HOST2
+	$IP netns add $NS_ROUTER
+}
+
+# delete network namespaces
+function delete_namespaces {
+	echo "Removing network namespaces..."
+	$IP netns delete $NS_HOST1
+	$IP netns delete $NS_HOST2
+	$IP netns delete $NS_ROUTER
+}
+
+# add veth interfaces to network namespaces
+function add_veths {
+	echo "Adding veth interfaces..."
+
+	# add veth interfaces
+	$IP link add $VETH_HOST11 type veth peer name $VETH_HOST12
+	$IP link add $VETH_HOST21 type veth peer name $VETH_HOST22
+
+	# set mac addresses of veth interfaces
+	$IP link set $VETH_HOST11 address $MAC_HOST11
+	$IP link set $VETH_HOST12 address $MAC_HOST12
+	$IP link set $VETH_HOST21 address $MAC_HOST21
+	$IP link set $VETH_HOST22 address $MAC_HOST22
+
+	# move veth interfaces to namespaces
+	$IP link set $VETH_HOST11 netns $NS_ROUTER
+	$IP link set $VETH_HOST12 netns $NS_HOST1
+	$IP link set $VETH_HOST21 netns $NS_ROUTER
+	$IP link set $VETH_HOST22 netns $NS_HOST2
+
+	# set veth interfaces up
+	$IP netns exec $NS_ROUTER $IP link set $VETH_HOST11 up
+	$IP netns exec $NS_HOST1 $IP link set $VETH_HOST12 up
+	$IP netns exec $NS_ROUTER $IP link set $VETH_HOST21 up
+	$IP netns exec $NS_HOST2 $IP link set $VETH_HOST22 up
+}
+
+# delete veth interfaces from network namespaces
+function delete_veths {
+	echo "Removing veth interfaces..."
+	$IP netns exec $NS_ROUTER $IP link delete $VETH_HOST11 type veth
+	$IP netns exec $NS_ROUTER $IP link delete $VETH_HOST21 type veth
+}
+
+# add ip addresses to veth interfaces
+function add_ips {
+	echo "Adding ip addresses to veth interfaces..."
+
+	# add ipv4 addresses to veth interfaces
+	$IP netns exec $NS_HOST1 $IP address add $IPV4_HOST1 dev $VETH_HOST12
+	$IP netns exec $NS_HOST2 $IP address add $IPV4_HOST2 dev $VETH_HOST22
+	$IP netns exec $NS_ROUTER $IP address add $IPV4_ROUTER1 \
+		dev $VETH_HOST11
+	$IP netns exec $NS_ROUTER $IP address add $IPV4_ROUTER2 \
+		dev $VETH_HOST21
+}
+
+# add routing that connects veth interfaces
+function add_routing {
+	echo "Adding routing..."
+
+	# enable routing on router
+	$IP netns exec $NS_ROUTER $SYSCTL -q -w net.ipv4.conf.all.forwarding=1
+
+	# set default routes on hosts
+	$IP netns exec $NS_HOST1 $IP route add default via ${IPV4_ROUTER1%/*} \
+		dev $VETH_HOST12
+	$IP netns exec $NS_HOST2 $IP route add default via ${IPV4_ROUTER2%/*} \
+		dev $VETH_HOST22
+}
+
+# set everything up
+function setup {
+	create_namespaces
+	add_veths
+	add_ips
+	add_routing
+}
+
+# tear everything down
+function tear_down {
+	delete_veths
+	delete_namespaces
+}
+
+# handle command line arguments
+case $1 in
+	"setup")
+		setup
+		;;
+	"teardown")
+		tear_down
+		;;
+	*)
+		echo "$0 setup|teardown"
+		;;
+esac
